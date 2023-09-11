1d6ac46ed2f26799e3466fbf404663a9eeb1ef69
samyarjahroodi <samyarghasemjahroodi@gmail.com>
2023-09-11 09:01:23 +0330

    url of the database added

--- jdbc/cw1jome/src/main/java/connection/JdbcConnection.java
+++ jdbc/cw1jome/src/main/java/connection/JdbcConnection.java
@@ -1,5 +1,15 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+            "postgres", "1381Iran");
+
+    public JdbcConnection() throws SQLException {
+    }
+
 
 }
